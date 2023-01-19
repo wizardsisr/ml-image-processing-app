6e6d9245be60823153442d3ad988b45d3d1fa31a
Tola Awofolu <oawofolu@pivotal.io>
2023-01-19 07:01:02 -0500

    Changes for python app

---
+++ app/analytics/cifar_cnn.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <div class="alert alert-box alert-info">
+#     <h3>Image Processing via Convolutional Neural Networks</h3>
+#     <a href="http://yann.lecun.com/exdb/publis/pdf/lecun-99.pdf" target="_blank">Original Paper by <b>Yann Lecun</b></a>
+#  </div>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import rcParams
+import warnings
+import seaborn as sns
+import tensorflow
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+import logging
+import pathlib
+import mlflow
+from mlflow import MlflowClient
+mlflow.set_tracking_uri('http://mlflow.tanzudatatap.ml')
+import warnings
+import tarfile
+import sys
+import importlib
+get_ipython().run_line_magic('pip', 'install hickle')
+import hickle as hkl
+import os
+import logging
+import traceback
+from io import StringIO
+from PIL import Image
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+warnings.filterwarnings('ignore')
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ## Utilities
+
+# In[2]:
+
+
+def get_run_for_artifacts(active_run_id):
+    runs = mlflow.search_runs(experiment_names=['Default'], filter_string="tags.mainartifacts='y'", max_results=1,
+                              output_format='list')
+    if len(runs):
+        return runs[0].info.run_id
+    else:
+        mlflow.set_tags({'mainartifacts':'y'})
+        return active_run_id
+
+def get_root_run(active_run_id, experiment_names=['Default']):
+    runs = mlflow.search_runs(experiment_names=experiment_names, filter_string="tags.runlevel='root'", max_results=1,
+                              output_format='list')
+    if len(runs):
+        parent_run_id = runs[0].info.run_id
+        mlflow.set_tags({'mlflow.parentRunId': parent_run_id})
+        return parent_run_id
+    else:
+        mlflow.set_tags({'runlevel':'root'})
+        return active_run_id
+
+
+def get_current_run():
+    last_active_run = mlflow.last_active_run()
+    return last_active_run.info.run_id if last_active_run else None
+
+
+# ## Upload dataset
+
+# In[3]:
+
+
+# Upload dataset to S3 via MlFlow
+def upload_dataset(dataset, dataset_url):
+    with mlflow.start_run(run_name='upload_dataset', nested=True) as active_run:
+        artifact_run_id = get_run_for_artifacts(active_run.info.run_id)
+
+        mlflow.environment_variables.MLFLOW_HTTP_REQUEST_TIMEOUT = '3600'
+        os.environ['MLFLOW_HTTP_REQUEST_TIMEOUT'] = '3600'
+
+        logging.info(f'Artifact run id is {artifact_run_id}')
+
+        client = MlflowClient()
+
+        mod = importlib.import_module(f'tensorflow.keras.datasets.{dataset}')
+        (training_data, training_labels), (test_data, test_labels) = mod.load_data()
+        training_data, test_data = training_data / 255.0, test_data / 255.0
+
+        hkl.dump({'training_data': training_data,
+                  'test_data': test_data,
+                  'training_labels': training_labels,
+                  'test_labels': test_labels},
+                 dataset, mode='w')
+
+        try:
+            client.log_artifact(artifact_run_id, dataset)
+            logging.info(f'File uploaded - run id {artifact_run_id}')
+        except Exception as e:
+            logging.error(f'Could not complete upload for run id {artifact_run_id} - error occurred: ', exc_info=True)
+            traceback.print_exc()
+
+
+mlflow.end_run()
+upload_dataset("cifar10", "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz")
+
+
+# ## Download DataSet
+
+def download_dataset(artifact, retries=2):
+    last_run_id = get_current_run()
+    root_run_id = get_root_run(last_run_id)
+    with mlflow.start_run(run_name='download_dataset', nested=True) as active_run:
+        artifact_run_id = get_run_for_artifacts(last_run_id)
+        download_path = 'downloads'
+        artifact_uri = f'runs:/{artifact_run_id}/{artifact}'
+        try:
+            artifact_path = mlflow.artifacts.download_artifacts(artifact_uri=artifact_uri,
+                                                                dst_path=download_path)
+            return artifact_path
+        except Exception as e:
+            if retries > 0:
+                download_dataset(artifact, retries=retries-1)
+            else:
+                logging.error(f'Could not complete download for artifact {artifact} - error occurred: ', exc_info=True)
+                traceback.print_exc()
+
+def download_model(model_name, model_flavor, best_run_id, retries=2):
+    with mlflow.start_run(run_name='download_model', nested=True) as active_run:
+        try:
+            client = MlflowClient()
+            versions = client.search_model_versions(f"name='{model_name}' and run_id='{best_run_id}'")
+            logging.info(f"In download model...search model results = {versions}")
+            if len(versions) and versions[0].current_stage.lower() != 'production':
+                version = versions[0].version
+                model = getattr(mlflow, model_flavor).load_model(f'models:/{model_name}/{version}')
+                return (model, version)
+            else:
+                logging.info(f"No suitable candidate model found for {model_name}...")
+                return None
+        except Exception as e:
+            if retries > 0:
+                logging.error(f"Could not download {model_name} - retrying...")
+                download_model(model_name, model_flavor, best_run_id, retries=retries-1)
+            else:
+                logging.error(f'Could not complete download for model {model_name} - error occurred: ', exc_info=True)
+                traceback.print_exc()
+
+
+# ## Train Model
+
+
+def train_model(model_name, model_flavor, model_stage, data, epochs=10):
+    with mlflow.start_run(run_name='train_model', nested=True) as active_run:
+        # Build and Compile Model
+        model = models.Sequential()
+        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(10))
+        model.summary()
+        model.compile(optimizer='adam',
+                      loss=tensorflow.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+
+        # Fit model
+        history = model.fit(data.get('training_data'), data.get('training_labels'), epochs=epochs,
+                            validation_data=(data.get('test_data'), data.get('test_labels')))
+
+        # Plot metrics
+        plt.plot(history.history['accuracy'], label='accuracy')
+        plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.ylim([0.5, 1])
+        plt.savefig("cifar_cnn_accuracy.png", bbox_inches='tight')
+        mlflow.log_artifact("cifar_cnn_accuracy.png")
+
+        # Log Metrics
+        test_loss, test_acc = model.evaluate(data.get('test_data'),  data.get('test_labels'), verbose=2)
+        mlflow.log_metric('testing_loss', test_loss)
+        mlflow.log_metric('testing_accuracy', test_acc)
+        getattr(mlflow, model_flavor).autolog(log_models=False)
+
+        # Log Model
+        #getattr(mlflow, model_flavor).log_model(model,
+        #                                        artifact_path=f'models:/{model_name}/{model_stage}',
+        #                                        registered_model_name=model_name,
+        #                                        await_registration_for=None)
+
+
+        getattr(mlflow, model_flavor).log_model(model,
+                                                artifact_path=f'runs:/{active_run.info.run_id}/{model_name}',
+                                                registered_model_name=model_name,
+                                                await_registration_for=None)
+
+
+        return model
+
+
+dataset_path = download_dataset('cifar10')
+data = hkl.load(dataset_path)
+train_model('cifar_cnn', 'tensorflow', 'None', data)
+
+
+# ## Evaluate Model
+
+def evaluate_model(experiment_names, model_name, model_flavor):
+    with mlflow.start_run(run_name='evaluate_model', nested=True) as active_run:
+        runs = mlflow.search_runs(experiment_names=experiment_names,
+                                  filter_string="attributes.run_name='train_model'",
+                                  order_by=['metrics.testing_accuracy DESC', 'metrics.testing_loss'],
+                                  max_results=1,
+                                  output_format='list')
+        logging.info(f"Best run found is...{runs}")
+        best_run_id = runs[0].info.run_id if len(runs) else None
+        if best_run_id is not None:
+            (model, version) = download_model(model_name, model_flavor, best_run_id)
+            logging.info(f"Found best model: {model}")
+
+            MlflowClient().transition_model_version_stage(
+                name=model_name,
+                version=version,
+                stage="Staging"
+            )
+
+evaluate_model(['Default'], 'cifar_cnn', 'tensorflow')
+
+
+# ## Make Prediction
+
+def predict(img, model_stage):
+    model = getattr(mlflow, 'tensorflow').load_model(f'models:/cifar_cnn/{model_stage}')
+    labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck' ]
+
+    img = img_to_array(img)
+    img = img.reshape(1, 32, 32, 3)
+    img = img.astype('float32')
+    img = img / 255.0
+
+    prediction_results = model.predict(img)
+    prediction = labels[np.argmax(prediction_results)]
+    logging.info(f'Predictions in order...list={prediction_results}, index={np.argmax(prediction_results)}, prediction={prediction}')
+    return prediction
+
+test_image = load_img('./test-images/bird7.png', target_size=(32, 32))
+predict(test_image, 'Staging')
+
+
+
+
