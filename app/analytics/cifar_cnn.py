502a3f7796e7df73ce454bd7eb420a684db23fe5
Tola Awofolu <oawofolu@pivotal.io>
2023-02-01 20:39:31 -0500

    Updated MLproject

--- app/analytics/cifar_cnn.py
+++ app/analytics/cifar_cnn.py
@@ -66,7 +66,7 @@ def get_current_run():
 # Upload dataset to S3 via MlFlow
 def upload_dataset(dataset, dataset_url=None):
     experiment_id = mlflow.get_experiment_by_name(os.environ.get('MLFLOW_EXPERIMENT_NAME') or 'Default')
-    with mlflow.start_run(run_name='upload_dataset', nested=True) as active_run:
+    with mlflow.start_run(run_name='upload_dataset') as active_run:
         artifact_run_id = get_run_for_artifacts(active_run.info.run_id)
 
         mlflow.environment_variables.MLFLOW_HTTP_REQUEST_TIMEOUT = '3600'
