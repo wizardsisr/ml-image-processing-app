a916ddc992705f2c63fcadf2a78aabde21391606
Tola Awofolu <oawofolu@pivotal.io>
2023-02-06 14:42:07 -0500

    New round of changes to support dummy Keras model

--- app/analytics/cifar_cnn.py
+++ app/analytics/cifar_cnn.py
@@ -245,8 +245,8 @@ def promote_model_to_staging(base_model_name, candidate_model_name, evaluation_d
 
             mlflow.evaluate(
                 candidate_model_info.model_uri,
-                pd.DataFrame({'data': _data.get('test_data').tolist(), 'labels': _data.get('test_labels').tolist()}),
-                targets="labels",
+                _data.get('test_data'),
+                targets=_data.get('test_labels'),
                 model_type="classifier",
                 validation_thresholds=thresholds,
                 baseline_model=base_model_info.model_uri,
