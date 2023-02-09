8b7fea1ddb67963e1009498705fff37c89d05b80
Tola Awofolu <oawofolu@pivotal.io>
2023-02-08 21:28:05 -0500

    Added SqlLite library

--- app/analytics/cifar_cnn.py
+++ app/analytics/cifar_cnn.py
@@ -221,7 +221,7 @@ def promote_model_to_staging(base_model_name, candidate_model_name, evaluation_d
             size, num_classes = test_labels.shape[0], 10
             dummy_data = pd.DataFrame({'x': np.random.randint(0, num_classes, size),
                                        'y': test_labels.reshape(size, )})
-            base_model = DummyClassifier(strategy="uniform").fit(dummy_data['x'], dummy_data['y'])
+            base_model = DummyClassifier().fit(dummy_data['x'], dummy_data['y'])
 
         # Generate and Save Evaluation Metrics
         curr_data = _tensors_to_1d_prediction_and_target(test_data, test_labels, candidate_model)
