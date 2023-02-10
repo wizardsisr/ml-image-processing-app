aa97cb5f2df3429977df1527e373d39ba6745445
Tola Awofolu <oawofolu@pivotal.io>
2023-02-09 20:14:50 -0500

    Fixed get_root_run logic

--- app/analytics/mlflow_utils.py
+++ app/analytics/mlflow_utils.py
@@ -30,7 +30,7 @@ def get_root_run(active_run_id=None):
 
 def start_new_root_run():
     root_run_id = get_root_run()
-    MlflowClient().set_terminated(get_root_run()) if root_run_id else True
+    MlflowClient().set_terminated(root_run_id) if root_run_id else True
     mlflow.set_tags({'runlevel': 'root'})
 
 
