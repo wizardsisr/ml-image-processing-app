20d0595cd13600b0441730a934f69697c52c3370
Tola Awofolu <oawofolu@pivotal.io>
2023-03-10 14:37:07 -0500

    Updated git clone url params

--- config/cifar/scripts/shell/deploy_python_inference_code.sh
+++ config/cifar/scripts/shell/deploy_python_inference_code.sh
@@ -1,10 +1,11 @@
 kubectl exec -it ${HOST} -n ${NAMESPACE} --  sh -c "rm -rf ${SHARED_PATH}/mlapp  ${SHARED_PATH}/mlappbase; \
-                   git clone ${GIT_REPO} ${SHARED_PATH}/mlapp; \
+                    git clone ${MLPIPELINE_GIT_REPO} ${SHARED_PATH}/mlapp; \
+                    mv ${SHARED_PATH}/mlapp/app ${SHARED_PATH}/mlapp/base_app; \
 
-                   curl -o vendor.tar.gz ${PYFUNC_VENDOR_URI}; \
-                   mkdir -p ${SHARED_PATH}/mlapp/_vendor; \
-                   tar -xvzf vendor.tar.gz -C ${SHARED_PATH}/mlapp/_vendor --strip-components=1; \
+                    curl -o vendor.tar.gz ${PYFUNC_VENDOR_URI}; \
+                    mkdir -p ${SHARED_PATH}/mlapp/_vendor; \
+                    tar -xvzf vendor.tar.gz -C ${SHARED_PATH}/mlapp/_vendor --strip-components=1; \
 
-                   git clone ${GIT_REPO} ${SHARED_PATH}/mlappbase; \
-                   mv ${SHARED_PATH}/mlappbase/config ${SHARED_PATH};
-                   sed -i \"s/XYZCHANGESETID/$(date +%s)/g; s/XYZDBSCHEMA/${DB_SCHEMA}/g;\" ${SHARED_PATH}/${DB_SCRIPT}"
+                    git clone ${GIT_REPO} ${SHARED_PATH}/mlappbase -b ${GIT_REPO_BRANCH}; \
+                    mv ${SHARED_PATH}/mlappbase/config ${SHARED_PATH};
+                    sed -i \"s/XYZCHANGESETID/$(date +%s)/g; s/XYZDBSCHEMA/${DB_SCHEMA}/g;\" ${SHARED_PATH}/${DB_SCRIPT}"
--- config/cifar/scripts/shell/deploy_python_training_code.sh
+++ config/cifar/scripts/shell/deploy_python_training_code.sh
@@ -1,4 +1,4 @@
-ssh -i ${SCP_PEM_PATH} ${USER}@${HOST} -o "StrictHostKeyChecking=no" "rm -rf ${SHARED_PATH}/mlapp ${SHARED_PATH}/mlappbase; \
+ssh -i ${SCP_PEM_PATH} ${USER}@${HOST} -o "StrictHostKeyChecking=no" "rm -rf ${SHARED_PATH}/mlapp ${SHARED_PATH}/mlappbase ${SHARED_PATH}/config; \
                    git clone ${MLPIPELINE_GIT_REPO} ${SHARED_PATH}/mlapp; \
                    mv ${SHARED_PATH}/mlapp/app ${SHARED_PATH}/mlapp/base_app; \
 
@@ -6,6 +6,6 @@ ssh -i ${SCP_PEM_PATH} ${USER}@${HOST} -o "StrictHostKeyChecking=no" "rm -rf ${S
                    mkdir -p ${SHARED_PATH}/mlapp/_vendor; \
                    tar -xvzf vendor.tar.gz -C ${SHARED_PATH}/mlapp/_vendor --strip-components=1; \
 
-                   git clone ${GIT_REPO} ${SHARED_PATH}/mlappbase; \
+                   git clone ${GIT_REPO} ${SHARED_PATH}/mlappbase -b ${GIT_REPO_BRANCH}; \
                    mv ${SHARED_PATH}/mlappbase/config ${SHARED_PATH};
                    sed -i \"s/XYZCHANGESETID/$(date +%s)/g; s/XYZDBSCHEMA/${DB_SCHEMA}/g;\" ${SHARED_PATH}/${DB_SCRIPT}"
