ace7e605f409447d33191bfe4116d7ff4bd932ea
Tola Awofolu <oawofolu@pivotal.io>
2023-03-10 07:54:16 -0500

    Updated shell script

--- config/cifar/scripts/shell/deploy_python_training_code.sh
+++ config/cifar/scripts/shell/deploy_python_training_code.sh
@@ -1,4 +1,4 @@
-ssh -i ${SCP_PEM_PATH} ${USER}@${HOST} "rm -rf ${SHARED_PATH}/mlapp ${SHARED_PATH}/mlappbase; \
+ssh -i ${SCP_PEM_PATH} ${USER}@${HOST} -o "StrictHostKeyChecking=no" "rm -rf ${SHARED_PATH}/mlapp ${SHARED_PATH}/mlappbase; \
                    git clone ${MLPIPELINE_GIT_REPO} ${SHARED_PATH}/mlapp; \
                    mv ${SHARED_PATH}/mlapp/app ${SHARED_PATH}/mlapp/base_app; \
 
