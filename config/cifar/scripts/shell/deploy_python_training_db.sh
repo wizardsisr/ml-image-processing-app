67cdc09fd42fee9f302259d40baa5be66e40579a
Tola Awofolu <oawofolu@pivotal.io>
2023-03-10 17:14:20 -0500

    Removed spurious escape characters

--- config/cifar/scripts/shell/deploy_python_training_db.sh
+++ config/cifar/scripts/shell/deploy_python_training_db.sh
@@ -1,1 +1,1 @@
-sed -i \"s/XYZCHANGESETID/$(date +%s)/g; s/XYZDBSCHEMA/${DB_SCHEMA}/g;\" ${SHARED_PATH}/${DB_SCRIPT}
+sed -i "s/XYZCHANGESETID/$(date +%s)/g; s/XYZDBSCHEMA/${DB_SCHEMA}/g;" ${SHARED_PATH}/${DB_SCRIPT}
