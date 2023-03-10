3b3dd0cef2bd67b94675adb5c1e86797be4f3702
Tola Awofolu <oawofolu@pivotal.io>
2023-03-10 08:18:45 -0500

    Updated permissions of pem in Vault

--- config/cifar/scripts/shell/deploy_python_training_code.sh
+++ config/cifar/scripts/shell/deploy_python_training_code.sh
@@ -1,3 +1,4 @@
+chmod 600 ${SCP_PEM_PATH}
 ssh -i ${SCP_PEM_PATH} ${USER}@${HOST} -o "StrictHostKeyChecking=no" "rm -rf ${SHARED_PATH}/mlapp ${SHARED_PATH}/mlappbase; \
                    git clone ${MLPIPELINE_GIT_REPO} ${SHARED_PATH}/mlapp; \
                    mv ${SHARED_PATH}/mlapp/app ${SHARED_PATH}/mlapp/base_app; \
