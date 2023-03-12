dd917f47ee753cb5e3035018d94a61b612366c18
Tola Awofolu <oawofolu@pivotal.io>
2023-03-12 05:13:12 -0400

    Added Postgres Inference cluster from GKE

--- config/cifar/scripts/sql/deploy_db_inference.sql
+++ config/cifar/scripts/sql/deploy_db_inference.sql
@@ -1,18 +1,16 @@
 --liquibase formatted sql
 --changeset pgadmin:XYZCHANGESETID
 CREATE EXTENSION 'plpython3u';
-CREATE OR REPLACE FUNCTION XYZDBSCHEMA.run_inference_task (model_name text, model_stage text, app_location text)
+CREATE OR REPLACE FUNCTION XYZDBSCHEMA.run_inference_task (model_name text, model_stage text, app_location text, git_repo text, git_branch text)
 RETURNS TEXT
 AS $$
     # container: plc_python3_shared
-    import os
-    import sys
-    import subprocess
-    import logging
-    logging.getLogger().addHandler(logging.StreamHandler())
-    logging.getLogger().addHandler(logging.FileHandler(f"{app_location}/debug.log"))
+    import os, sys, subprocess, logging, git
     try:
-        sys.path.append(f'{app_location}')
+        repo = git.Repo.clone_from(f"{git_repo} --single-branch", f"{app_location}/mlapp", branch=git_branch)
+        logging.getLogger().addHandler(logging.StreamHandler())
+        logging.getLogger().addHandler(logging.FileHandler(f"{app_location}/mlapp/debug.log"))
+        sys.path.append(f'{app_location}/mlapp')
         if sys.modules.get('app.analytics.cifar_cnn'):
             del sys.modules['app.analytics.cifar_cnn']
         if sys.modules.get('app.analytics.config'):
