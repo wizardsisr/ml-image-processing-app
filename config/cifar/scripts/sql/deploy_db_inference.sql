3eeed58f816588394033c99c083bfbf82882f499
Tola Awofolu <oawofolu@pivotal.io>
2023-03-13 07:33:44 -0400

    Fixes for inference sql script

--- config/cifar/scripts/sql/deploy_db_inference.sql
+++ config/cifar/scripts/sql/deploy_db_inference.sql
@@ -5,18 +5,18 @@ CREATE OR REPLACE FUNCTION XYZDBSCHEMA.run_inference_task (model_name text, mode
 RETURNS TEXT
 AS $$
     # container: plc_python3_shared
-    import os, sys, subprocess, logging, git
+    import os, sys, subprocess, logging
     try:
-        repo = git.Repo.clone_from(f"{git_repo} --single-branch", f"{app_location}/mlapp", branch=git_branch)
+        dir = f"{os.path.expanduser('~')}/{app_location}"
+        os.environ['MLFLOW_TRACKING_URI'] = mlflow_tracking_uri
+        os.system(f'git clone -v --branch={git_branch} "{git_repo}" --single-branch {dir}')
         logging.getLogger().addHandler(logging.StreamHandler())
-        logging.getLogger().addHandler(logging.FileHandler(f"{app_location}/mlapp/debug.log"))
-        sys.path.append(f'{app_location}/mlapp')
-        if sys.modules.get('app.analytics.cifar_cnn'):
-            del sys.modules['app.analytics.cifar_cnn']
-        if sys.modules.get('app.analytics.config'):
-            del sys.modules['app.analytics.config']
+        logging.getLogger().addHandler(logging.FileHandler(f"{dir}/debug.log"))
+        sys.path.append(f'{dir}') if dir not in sys.path else True
+        sys.modules.pop('app.analytics.cifar_cnn') if sys.modules.get('app.analytics.cifar_cnn') else True
+        sys.modules.pop('app.analytics.config') if sys.modules.get('app.analytics.config') else True
         from app.analytics import cifar_cnn, config
-        return cifar_cnn.predict(img, model_name, model_stage)
+        return cifar_cnn.predict(None, model_name, model_stage)
     except subprocess.CalledProcessError as e:
         return e.output
 $$
