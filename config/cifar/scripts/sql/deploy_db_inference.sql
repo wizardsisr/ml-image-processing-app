1d2fafdfd8358fd503d6f02e5376ca830af251fd
Tola Awofolu <oawofolu@pivotal.io>
2023-03-13 20:37:26 -0400

    Updated inference SQL script

--- config/cifar/scripts/sql/deploy_db_inference.sql
+++ config/cifar/scripts/sql/deploy_db_inference.sql
@@ -1,7 +1,12 @@
 --liquibase formatted sql
 --changeset pgadmin:XYZCHANGESETID
 CREATE EXTENSION plpython3u;
-CREATE OR REPLACE FUNCTION XYZDBSCHEMA.run_inference_task (model_name text, model_stage text, app_location text, git_repo text, git_branch text)
+CREATE OR REPLACE FUNCTION XYZDBSCHEMA.run_inference_task (model_name text,
+    model_stage text,
+    app_location text,
+    mlflow_tracking_uri text,
+    git_repo text,
+    git_branch text)
 RETURNS TEXT
 AS $$
     # container: plc_python3_shared
