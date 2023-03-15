76881397a97ea7e358d99e077a736ed8a2ac6c52
Tola Awofolu <oawofolu@pivotal.io>
2023-03-15 18:57:34 -0400

    Updated API params

--- app/analytics/cifar_cnn_greenplum.py
+++ app/analytics/cifar_cnn_greenplum.py
@@ -65,6 +65,6 @@ def predict(img, model_name, model_stage, schema='public'):
     result = db.apply(lambda: inference_function(img, model_name, model_stage, 'mlapp', 'http://mlflow.tanzumlai.com',
                                                  'https://github.com/agapebondservant/ml-image-processing-app.git',
                                                  'gp-main'))
-    logging.info(f"Result = {result[0][0]}")
-    return result[0][0]
+    logging.info(f"Result = {type(result)}")
+    return result
 
