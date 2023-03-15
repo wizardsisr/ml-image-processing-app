8af87f031e3935c92d6caf5f545cb972c32fa9df
Tola Awofolu <oawofolu@pivotal.io>
2023-03-15 09:02:55 -0400

    Merged changes

--- app/analytics/preloader.py
+++ app/analytics/preloader.py
@@ -1,4 +1,4 @@
 import sys
 import os
-if os.path.exists('_vendor'):
-    sys.path.append('_vendor')
+if os.path.exists(f"{os.environ.get('shared_app_path')}/_vendor"):
+    sys.path.append(f"{os.environ.get('shared_app_path')}/_vendor")
