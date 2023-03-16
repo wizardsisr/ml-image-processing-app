e53f7a86fd5d74fa801228bc691bc9d6f9a849fd
Tola Awofolu <oawofolu@pivotal.io>
2023-03-16 06:16:17 -0400

    Removed pyarrow serialization

--- app/analytics/cifar_cnn_greenplum.py
+++ app/analytics/cifar_cnn_greenplum.py
@@ -65,6 +65,6 @@ def predict(img, model_name, model_stage, schema='public'):
     result = db.apply(lambda: inference_function(img, model_name, model_stage, 'mlapp', 'http://mlflow.tanzumlai.com',
                                                  'https://github.com/agapebondservant/ml-image-processing-app.git',
                                                  'gp-main'))
-    logging.info(f"Result = {result}, {next(iter(result))}")
+    logging.info(f"Result = {result}")
     return result
 
