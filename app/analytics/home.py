f8bb9deb711aaefb379d39d5b37b02ef0d559448
Tola Awofolu <oawofolu@pivotal.io>
2023-03-17 04:05:31 -0400

    Updated App UI

--- app/analytics/home.py
+++ app/analytics/home.py
@@ -54,21 +54,22 @@ with tab1:
         with col1:
             st.image(cifar_img, width=200)
         with col2:
-            st.header('Please wait...')
-            st.text('(Loading model)')
-            prediction = cifar_cnn_greenplum.predict(cifar_img, config.model_name, config.model_stage)
-            if prediction:
-                st.markdown(f"Predicted Label:<br/> <span class='predictedlabel'>{prediction}</span>",
-                            unsafe_allow_html=True)
-                metrics = cifar_cnn.get_metrics()
-                logging.info(f"Metrics = {metrics}")
-                st.markdown(f"<br/>F-1 metric:<br/> <span class='metriclabel'>{metrics.get('f1_score') or 'None available'}</span>",
-                            unsafe_allow_html=True)
-                st.markdown(f"<br/>Accuracy metric:<br/> <span class='metriclabel'>{metrics.get('accuracy_score') or 'None available'}</span>",
-                            unsafe_allow_html=True)
-            else:
-                st.header('Please wait...')
-                st.text('(Training is in progress)')
+            placeholder = st.empty()
+            placeholder.markdown("<div class='blinking'>Please wait...&nbsp;</div>", unsafe_allow_html=True)
+            with placeholder.container():
+                prediction = cifar_cnn_greenplum.predict(cifar_img, config.model_name, config.model_stage)
+                if prediction:
+                    st.markdown(f"Predicted Label:<br/> <span class='predictedlabel'>{prediction}</span>",
+                                unsafe_allow_html=True)
+                    metrics = cifar_cnn.get_metrics()
+                    logging.info(f"Metrics = {metrics}")
+                    st.markdown(f"<br/>F-1 metric:<br/> <span class='metriclabel'>{metrics.get('f1_score') or 'None available'}</span>",
+                                unsafe_allow_html=True)
+                    st.markdown(f"<br/>Accuracy metric:<br/> <span class='metriclabel'>{metrics.get('accuracy_score') or 'None available'}</span>",
+                                unsafe_allow_html=True)
+                else:
+                    st.header('Please wait...')
+                    st.text('(Training is in progress)')
 
 # MRI
 with tab2:
